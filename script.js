e3d8b70857fa880af07cc59f617bb189ab2be999
Julia <yulyakonoval1998@gmail.com>
2021-03-02 18:42:14 +0200

    about section and the slider with pets created

---
+++ script.js
@@ -0,0 +1,50 @@
+gsap.registerPlugin(ScrollTrigger);
+let tl = gsap.timeline();
+
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+
+  // Toggle Nav
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+    if (nav.classList.contains("nav-active")) {
+      tl.fromTo(
+        navLinks,
+        {
+          opacity: 0,
+          x: 100,
+        },
+        {
+          duration: 1,
+          delay: 1,
+          stagger: 0.2,
+          opacity: 1,
+          x: 0,
+          ease: "elastic.out(1, 0.3)",
+        },
+        "-0.35"
+      );
+    } else {
+      tl.clear();
+    }
+
+    burger.classList.toggle("toggle");
+  });
+};
+
+navSlide();
+
+$(document).ready(function () {
+  $(".carousel").slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    arrows: true,
+    prevArrow:
+      "<button type='button' class='slick-prev'></button>",
+    nextArrow:
+      "<button type='button' class='slick-next'></button>",
+  });
+});
